5a95fa68427edd6ed54ec42e0a18b41925b71150
Abu Qader <48742992+aspctu@users.noreply.github.com>
2023-07-09 12:02:25 -0500

    clean up

--- recorder.py
+++ recorder.py
@@ -1,48 +1,61 @@
 import datetime
 import hashlib
 import time
+from pathlib import Path
 
 from pydub import AudioSegment
+from rich.console import Console
 from speech_recognition import AudioData, Microphone, Recognizer
 
-from constants import AUDIO_FORMAT, SAMPLING_RATE, CHANNELS, PHRASE_TIME_LIMIT
+from constants import AUDIO_FORMAT, CHANNELS, PHRASE_TIME_LIMIT, SAMPLING_RATE
+
+console = Console()
 
 
 class Recorder:
     def __init__(self, transcription_queue, stop_event):
         self.transcription_queue = transcription_queue
         self.stop_event = stop_event
+        self.recognizer = Recognizer()
+        self.recognizer.energy_threshold = 1000
+        self.recognizer.dynamic_energy_threshold = False
+    
+    def serialize_buffer(self, audio: AudioData) -> Path:
+        def _generate_filename() -> str:
+            timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+            return f"{hashlib.sha1(timestamp.encode()).hexdigest()}.{AUDIO_FORMAT}"
+        
+        raw_data = audio.get_raw_data(
+            convert_rate=SAMPLING_RATE,
+            convert_width=2,
+        )
+        audio_segment = AudioSegment(
+            data=raw_data,
+            sample_width=2,
+            frame_rate=SAMPLING_RATE,
+            channels=CHANNELS
+        )
+
+        serialize_path = Path(_generate_filename())
+        audio_segment.export(serialize_path, format=AUDIO_FORMAT)
+        return serialize_path
 
     def record(self):
-        recognizer = Recognizer()
-        recognizer.energy_threshold = 1000
-        recognizer.dynamic_energy_threshold = False
-
-        # Create a callback to handle audio data
         def record_callback(recognizer: Recognizer, audio: AudioData) -> None:
-            timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-            filename = hashlib.sha1(timestamp.encode()).hexdigest() + "." + AUDIO_FORMAT
-
-            # get raw audio data and save to file
-            raw_data = audio.get_raw_data(convert_rate=SAMPLING_RATE, convert_width=2)
-            audio_segment = AudioSegment(
-                data=raw_data,
-                sample_width=2,
-                frame_rate=SAMPLING_RATE,
-                channels=CHANNELS
-            )
-
-            audio_segment.export(filename, format=AUDIO_FORMAT)
-
-            # Add the filename to the queue for transcription
+            filename = self.serialize_buffer(audio)
             self.transcription_queue.put(filename)
 
         # Start listening in the background
         with Microphone(sample_rate=SAMPLING_RATE) as source:
-            recognizer.adjust_for_ambient_noise(source)
-        
-        recognizer.listen_in_background(source, record_callback, phrase_time_limit=PHRASE_TIME_LIMIT)
-        print("Starting recording...")
+            self.recognizer.adjust_for_ambient_noise(source)
+
+        self.recognizer.listen_in_background(
+            source,
+            record_callback,
+            phrase_time_limit=PHRASE_TIME_LIMIT
+        )
+
+        console.print("[green]Starting recording... [/green]")
 
         # Keep this thread alive until stop event is set
         while not self.stop_event.is_set():
--- transcriber.py
+++ transcriber.py
@@ -3,18 +3,21 @@ import threading
 from pathlib import Path
 
 from constants import WHISPER_MODEL
+from rich.console import Console
+
+console = Console()
 
 class Whisper:
     def __init__(self, model_path):
         self.model_path = Path(model_path)
 
-    def transcribe(self, audio_file):
+    def transcribe(self, audio_file: str) -> str:
         audio_file_path = Path(audio_file)
         txt_file_path = Path(f"{audio_file_path}.txt")
         try:
             subprocess.run(
                 [
-                    './whisper.cpp/main', '-m', str(self.model_path), '-f', 
+                    './whisper.cpp/main', '-m', str(self.model_path), '-f',
                     str(audio_file_path), '--output-txt'
                 ],
                 check=True,
@@ -22,17 +25,16 @@ class Whisper:
                 capture_output=True
             )
         except subprocess.CalledProcessError as e:
-            print(f"Whisper command failed: {e}")
+            console.print(f"[red]Whisper command failed: {e}[/red]")
             return None
 
         # Read in the generate file txt
-        with txt_file_path.open("r") as f:
-            transcription = f.read()
+        transcription = txt_file_path.read_text()
         
         # Remove the generated file
         txt_file_path.unlink(missing_ok=True)
 
-        return transcription.strip() 
+        return transcription.strip()
 
 class Transcriber:
     def __init__(self, transcription_queue, stop_event, output_file):
@@ -42,6 +44,11 @@ class Transcriber:
         self.file_lock = threading.Lock()
         self.output_file = Path(output_file)
 
+    def write(self, transcription: str) -> None:
+        with self.file_lock:
+            with self.output_file.open('a') as output_file:
+                output_file.write(transcription + '\n')
+
     def transcribe(self):
         while not self.stop_event.is_set():
             if not self.transcription_queue.empty():
@@ -49,14 +56,12 @@ class Transcriber:
 
                 try:
                     transcript = self.whisper.transcribe(audio_file)
-                    print(transcript)
+                    console.print(f"[blue]{transcript} [/blue]")
                 except Exception as e:
-                    print(f"Failed to transcribe {audio_file}: {e}")
+                    console.print(f"[red]Failed to transcribe {audio_file}: {e}[/red]")
                     continue
 
-                with self.file_lock: 
-                    with self.output_file.open('a') as output_file:
-                        output_file.write(transcript + '\n')
+                self.write(transcription=transcript)
 
                 Path(audio_file).unlink(missing_ok=True)
                 self.transcription_queue.task_done()
